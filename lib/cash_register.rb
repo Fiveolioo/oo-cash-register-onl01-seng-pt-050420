d0fe5bd01c864b9c6e7bc3db00f4b54ce67e0551
Richard Cho <96richardohc@gmail.com>
2020-05-28 07:26:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :item
+ 
+  def initialize(discount=0)
+  @total = 0
+  @discount = discount
+  end
+  
+  def add_item(item, cost, quantity=1)
+    self.total += cost * quantity
+  end
+  
+  def apply_discount
+    self.total - (self.total * @discount / 100)
+  end
+end
